557e6c1edf069ab96fed08b2aca4edd9be5a20db
Hunvely <jihoon9611@gmail.com>
2024-03-20 18:02:26 +0900

    feat. Board Entity Mapping

--- src/main/java/com/rod/api/board/Board.java
+++ src/main/java/com/rod/api/board/Board.java
@@ -1,4 +1,23 @@
 package com.rod.api.board;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name="board")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(exclude = {"id"})
 public class Board {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String boardName;
+    private String boardType;
+
+    @Builder(builderMethodName = "builder")
+    public Board(String boardName, String boardType) {
+        this.boardName = boardName;
+        this.boardType = boardType;
+    }
 }
