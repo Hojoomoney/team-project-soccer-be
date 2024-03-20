7293191dd4a4bdd424b2a2ad0f6a9a5e23697406
Hojoomoney <kimhojoo51@gmail.com>
2024-03-20 19:43:25 +0900

    .

--- src/main/java/com/rod/api/user/UserController.java
+++ src/main/java/com/rod/api/user/UserController.java
@@ -17,7 +17,18 @@ import java.util.Scanner;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
-
+    @PostMapping("/login")
+    public Map<String, ?> username(@RequestBody Map<String, ?> map){
+        String username = (String) map.get("username");
+        String password = (String) map.get("password");
+        System.out.println("리퀘스트가 가져온 이름 : " + username);
+        System.out.println("리퀘스트가 가져온 비밀번호 : " + password);
+        Map<String, String> respMap = new HashMap<>();
+        respMap.put("username", username);
+        respMap.put("password", password);
+        return respMap;
+    }
 
 }
