fcec1447fde356ed071f17fae93faaa4178d1fa1
GiselePark <hanied04@gmail.com>
2024-03-21 17:33:45 +0900

    new project

--- src/main/java/com/rod/api/product/Product.java
+++ src/main/java/com/rod/api/product/Product.java
@@ -1,4 +1,13 @@
 package com.rod.api.product;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(exclude = {"id"})
+
 public class Product {
 }
