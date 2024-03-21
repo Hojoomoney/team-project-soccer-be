5f9d79fca05515080c0b43c340376907a836b08b
GiselePark <hanied04@gmail.com>
2024-03-21 18:18:15 +0900

    Create Entity product

--- src/main/java/com/rod/api/product/Product.java
+++ src/main/java/com/rod/api/product/Product.java
@@ -1,13 +1,29 @@
 package com.rod.api.product;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity(name="products")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @ToString(exclude = {"id"})
 
 public class Product {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer pno;
+    private String name;
+    private String company;
+    private Integer price;
+
+    @Builder(builderMethodName = "builder")
+    public Product(int id, int pno, String name, String company, int price) {
+        this.id = id;
+        this.pno = pno;
+        this.name = name;
+        this.company = company;
+        this.price = price;
+    }
 }
