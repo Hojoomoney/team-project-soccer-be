a9c36dc91e33465871b8cd70aa5ef6d11a1bc572
Hojoomoney <kimhojoo51@gmail.com>
2024-03-25 18:14:04 +0900

    refactor : UserController.java (login)

--- src/main/java/com/rod/api/user/UserController.java
+++ src/main/java/com/rod/api/user/UserController.java
@@ -2,16 +2,15 @@ package com.rod.api.user;
 
 import com.rod.api.enums.Messenger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -21,13 +20,17 @@ public class UserController {
     private final UserRepository userRepository;
     @PostMapping("/api/login")
     public Map<String, ?> username(@RequestBody Map<?, ?> map){
+        Map<String, Messenger> respMap = new HashMap<>();
         String username = (String) map.get("username");
         String password = (String) map.get("password");
-        System.out.println("리퀘스트가 가져온 이름 : " + username);
-        System.out.println("리퀘스트가 가져온 비밀번호 : " + password);
-        Map<String, String> respMap = new HashMap<>();
-        respMap.put("username", username);
-        respMap.put("password", password);
+        User optUser = userRepository.findByUsername(username).orElse(null);
+        if(optUser == null){
+            respMap.put("message", Messenger.FAIL);
+        } else if(!password.equals(optUser.getPassword())){
+            respMap.put("message", Messenger.WRONG_PASSWORD);
+        } else {
+            respMap.put("message", Messenger.SUCCESS);
+        }
         return respMap;
     }
 
@@ -43,7 +46,7 @@ public class UserController {
                 .job((String) map.get("job"))
                 .build());
         System.out.println("DB 에 저장된 User 정보 : " + user);
-        respMap.put("result", Messenger.SUCCESS);
+        respMap.put("message", Messenger.SUCCESS);
         return respMap;
     }
 
